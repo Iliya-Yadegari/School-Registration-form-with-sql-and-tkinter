c5cf14c02a07771e797509ee9aa712f316513902
Iliya-Yadegari <naruto3@email.com>
2021-04-29 21:35:08 +0100

    Update SchoolRegFormModule.py

--- SchoolRegFormModule.py
+++ SchoolRegFormModule.py
@@ -1,12 +1,18 @@
 import mysql.connector as msc
 
-def column_save():
+def column_save(name,LastName,birthDate,gender,ethnicity,emailAddress,entryYear,grade,semester,appliedOrNot,streetAddress,streetAddressLine2,city,state,zipCode,HomePhoneNum,CellPhoneNum):
 
     mydb = msc.connect(
         host = 'localhost',
         user = 'root',
         password = '',
-        database = 'myfirstdatabase'
+        database = 'schoolregform'
         )
     
     mycursor = mydb.cursor()
+
+    sqlCommand = 'INSERT INTO student (First Name,Last Name,birth date,gender,ethnicity,email address,Entry year,grade,semester,appliedOrNot,street address,street address line 2,city,State/Province,Postal/zip code,Home Phone Number,Cell Phone number) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+    values = (name,LastName,birthDate,gender,ethnicity,emailAddress,entryYear,grade,semester,appliedOrNot,streetAddress,streetAddressLine2,city,state,zipCode,HomePhoneNum,CellPhoneNum)
+    
+    mycursor.execute(sqlCommand,values)
+    mydb.commit()
