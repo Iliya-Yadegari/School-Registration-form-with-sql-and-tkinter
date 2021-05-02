59dfbaf19a45a352ccd052574aa48aeb269a26ae
Iliya-Yadegari <naruto3@email.com>
2021-05-02 16:27:43 +0100

    Update SchoolRegFormModule.py

--- SchoolRegFormModule.py
+++ SchoolRegFormModule.py
@@ -12,7 +12,9 @@ def column_save(name,LastName,birthDate,gender,ethnicity,emailAddress,entryYear,
     mycursor = mydb.cursor()
 
     sqlCommand = 'INSERT INTO student (First Name,Last Name,birth date,gender,ethnicity,email address,Entry year,grade,semester,appliedOrNot,street address,street address line 2,city,State/Province,Postal/zip code,Home Phone Number,Cell Phone number) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
-    values = (name,LastName,birthDate,gender,ethnicity,emailAddress,entryYear,grade,semester,appliedOrNot,streetAddress,streetAddressLine2,city,state,zipCode,HomePhoneNum,CellPhoneNum)
+    values = (name,LastName,birthDate,gender,ethnicity,emailAddress,entryYear,grade,
+              semester,appliedOrNot,streetAddress,streetAddressLine2,city,state,
+              zipCode,HomePhoneNum,CellPhoneNum)
     
     mycursor.execute(sqlCommand,values)
     mydb.commit()
