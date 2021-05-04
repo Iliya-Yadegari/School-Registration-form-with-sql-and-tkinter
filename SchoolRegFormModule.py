4c1e4ff80fc5a17e1530350c3c8e882089c8929a
Iliya-Yadegari <naruto3@email.com>
2021-05-04 21:07:05 +0100

    Update SchoolRegFormModule.py

--- SchoolRegFormModule.py
+++ SchoolRegFormModule.py
@@ -11,8 +11,8 @@ def column_save(name,LastName,birthDate,gender,ethnicity,emailAddress,entryYear,
     
     mycursor = mydb.cursor()
 
-    sqlCommand = 'INSERT INTO resultsv2 (Name,LastName,Birthdate,Gender,Ethnicity,EntryYear,Grade,Semester,appliedOrNot,StreetAddress,StreetAddressLine2,City,State,PostalNum,HomePhoneNum,CellPhoneNum) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
-    values = (name,LastName,birthDate,gender,ethnicity,emailAddress,entryYear,grade,
+    sqlCommand = 'INSERT INTO resultsv2 (Name,LastName,Birthdate,Gender,Ethnicity,EntryYear,Grade,Semester,appliedOrNot,StreetAddress,StreetAddressLine2,City,State,PostalNum,HomePhoneNum,CellPhoneNum) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+    values = (name,LastName,birthDate,gender,ethnicity,entryYear,grade,
               semester,appliedOrNot,streetAddress,streetAddressLine2,city,state,
               zipCode,HomePhoneNum,CellPhoneNum)
     
